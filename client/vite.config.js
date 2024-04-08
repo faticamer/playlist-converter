69910738bd10ef60010c28672bf749ee8731adb1
faticamer <faticamer17@gmail.com>
2024-04-08 20:53:05 +0200

    Add Docker config and update Vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/)
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 5173,
+   strictPort: true,
+  },
+  server: {
+   port: 5173,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:5173",
+  },
+ });
