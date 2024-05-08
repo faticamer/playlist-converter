6fdd3194078d59c33692f305908e4f78483259ed
faticamer <faticamer17@gmail.com>
2024-05-09 00:00:55 +0200

    Re-design of Convert button

--- client/src/pages/Convert.jsx
+++ client/src/pages/Convert.jsx
@@ -83,7 +83,7 @@ const Convert = ({user}) => {
           await convert(youtubePlaylistId);
 
           ids = retrieveFromLocalStorage('items')
-          const parsedIdsFromLocalStorage = ids.replace('[', '').replace(']', '');        
+          const parsedIdsFromLocalStorage = ids.replace('[', '').replace(']', '');
           const idArray = parsedIdsFromLocalStorage.split(',') // this created an array
           const response = await getTracksInfo(idArray)
 
@@ -186,7 +186,7 @@ const Convert = ({user}) => {
             <NavigationBar user={user}/>
           </div>
           <div className='flex flex-row items-center justify-center'>
-          <div className='text-white w-1/3 h-[65vh] border-2 border-zinc-700 bg-spotifyDarkGrey rounded-2xl mt-6 ml-6 p-3 overflow-auto'>
+          <div className='text-white w-1/3 h-[70vh] border-2 border-zinc-700 bg-spotifyDarkGrey rounded-2xl mt-6 ml-6 p-3 overflow-auto'>
             <h1 className='pb-3'>Songs in your New Playlist</h1>
             <div>
               {(items.length > 0) ?  <InfoPane list={items} /> : <div>No data currently available</div>}
@@ -198,7 +198,7 @@ const Convert = ({user}) => {
                   <input onChange={handleInputChange} type='text' placeholder='Place your URL' className='p-4 w-full rounded-md bg-zinc-800 text-white text-center border border-green-800 focus:outline-none focus:bg-zinc-700 onfocus="this' id='convertInput' />
               </div>
               <div className='flex flex-col items-center justify-center w-2/5 m-5 pt-4'>
-                <button onClick={callConvert} disabled={isLoading} className='rounded-2xl w-1/2 font-normal border-2 border-green-800 p-3 dark:text-white hover:bg-zinc-600'>{ isLoading ? 'Loading...' : 'Start Converting' }</button>                
+                <button onClick={callConvert} disabled={isLoading} className='rounded-full font-normal border-2 border-green-800 p-3 dark:text-white hover:bg-zinc-600'>{ isLoading ? 'Loading...' :<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='size-10'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#1db954" d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9V344c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z"/></svg><path fill="#1db954" d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9V344c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z"/></svg> }</button>
               </div>
               <div>
                 { isLoading ? 
@@ -214,7 +214,7 @@ const Convert = ({user}) => {
                 }
               </div>
             </div>
-            <div className='text-white w-1/3 h-[65vh] border-2 border-zinc-700 bg-spotifyDarkGrey rounded-2xl mt-6 mr-6 p-3 overflow-auto'>
+            <div className='text-white w-1/3 h-[70vh] border-2 border-zinc-700 bg-spotifyDarkGrey rounded-2xl mt-6 mr-6 p-3 overflow-auto'>
               <h1 className='pb-3'>Your library</h1>
               {(library.length > 0) ?  <InfoPane list={library} /> : <div>No data currently available</div>}
             </div>
