5b73bd83443d83c4c16ccd0c52ca2404680ff210
faticamer <faticamer17@gmail.com>
2024-05-25 23:45:05 +0200

    Re-design of navigation bar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -12,7 +12,8 @@ const NavigationBar = ({user}) => {
         <nav className='relative mx-auto p-3 text-white bg-spotifyDarkGrey'>
             <div className='flex items-center justify-between'>
                 <div className='pt-2'>
-                    <a href="/" className='text-3xl font-bold nunito-sans-bold ml-6'>Playlistify</a>
+                    {user ? <a href="/" className='text-3xl font-bold nunito-sans-bold ml-6'>Playlistify <span className='pl-2 text-md font-normal text-stone-500'> | </span><span className='pl-2 text-2xl font-normal'>Welcome, <span className='text-red-600'>{user}!</span></span></a> : 
+                    <a href="/" className='text-3xl font-bold nunito-sans-bold ml-6'>Playlistify <span className='pl-2 text-md font-normal text-stone-500'> | </span><span className='pl-2 text-2xl font-normal'>Welcome, Guest!</span></a>}
                 </div>
                 <div className='flex flex-row justify-between items-center gap-5 nunito-sans-regular'>
                     <div onClick={openSourceCode}>
