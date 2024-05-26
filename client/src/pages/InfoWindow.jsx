a7be4ea9ce1bdacfbc4b8686ac1f10bcd3e5318c
faticamer <faticamer17@gmail.com>
2024-05-26 23:56:33 +0200

    Changed info template text

--- client/src/pages/InfoWindow.jsx
+++ client/src/pages/InfoWindow.jsx
@@ -12,12 +12,11 @@ const InfoWindow = ({user}) => {
             <div className="flex flex-col items-center justify-center h-[75vh] w-full nunito-sans-regular">
                 <h1 className="text-white text-2xl pb-3">About Playlistify</h1>
                 {user ? <div></div> : <ConnectButton user={user}/>}
-                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Disclaimer: Spotify will ask for lots of permissions to your account. All permissions are related to features on Playlistify will not store your Spotify data. All of your Spotify related data is viewed by your eyes only. Read the Privacy Policy if you feel uneasy.</p>
-                <p className="px-12 pt-12 md:w-3/4 w-full text-center text-zinc-300">Playlistify views your Spotify playback history, just in case you want to add tracks that you recently played to your tier list</p>
-                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Playlistify views your Spotify user account data to check whether the app works correctly, and to automatically fill in the name of your account in the tier list. Spotify will ask you for other data too, but this app only uses the name.</p>
-                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Playlistify views your recently played tracks, liked items, followed artists, saved items and playlists in your library (ones that you also made) to use as items in the tier list</p>
-                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Playlistify asks for permission to create and edit playlists so that you can create a playlist out of your tier list once you are finished</p>
-                <p className="px-12 pt-5 md:w-3/4 w-full text-center text-zinc-300">Playlistify asks for permission to view your top artists and tracks to quickly add them to your tier list</p>
+                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Disclaimer: Spotify will ask for lots of permissions to your account. All permissions are related to features on Playlistify will not store your Spotify data. All of your Spotify related data is viewed by your eyes only. Read the Privacy Policy if you feel uneasy.</p>                
+                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Playlistify views your Spotify user account data to check whether the app works correctly, and to automatically fill in the name of your account in the welcome message. Spotify will ask you for other data too, but this app only uses the name.</p>
+                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Playlistify views your library, individual playlists (ones that you also made and generated), items in playlists</p>
+                <p className="px-12 pt-6 md:w-3/4 w-full text-center text-zinc-300">Playlistify asks for permission to create and edit playlists in terms of naming</p>
+                <p className="px-12 pt-5 md:w-3/4 w-full text-center text-zinc-300">Playlistify will work seamlessly after all permissions are granted.</p>
             </div>
             <div className="pt-6">
                 <Footer />
