31c9e2b5531aa0faacdff1abaa24a53e3b9450fb
faticamer <faticamer17@gmail.com>
2024-05-30 23:58:28 +0200

    Add a few ternary operators to display 0 if it is present in track duration

--- client/src/components/InfoPane.jsx
+++ client/src/components/InfoPane.jsx
@@ -6,15 +6,20 @@ function InfoPane ({list}) {
     
     if(marker === 'tracks') {
         const newArr = removeMarker(list)
-        return (                
+        console.log(newArr)
+        return (
             <div>
               <ul className = 'pt-3'>
                 {newArr.map((item, index) => (
                   <li className = 'text-lg' key={index}>
                     <h1>
-                        {item.artist}
+                        {index}
+                        . {item.artist}
                         {item.name}
-                        <b> {Math.floor(item.duration_ms / (1000 * 60))}:{Math.floor((item.duration_ms % (1000 * 60)) / 1000)}</b>
+                        <b> {Math.floor(item.duration_ms / (1000 * 60)) + ":" + 
+                        (Math.floor((item.duration_ms % (1000 * 60)) / 1000) < 10 ? "0" + 
+                        Math.floor((item.duration_ms % (1000 * 60)) / 1000) : 
+                        Math.floor((item.duration_ms % (1000 * 60)) / 1000))}</b>
                     </h1>
                   </li>
                 ))}
@@ -23,6 +28,7 @@ function InfoPane ({list}) {
         );
     } else if(marker === 'library') {
         const newArr = removeMarker(list)
+        console.log(newArr)
         return (
             <div>
                 <ul className = 'pt-3'>
