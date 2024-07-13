2ffad265f918fe0aa01e9bd6decc61c82e3f0bea
faticamer <faticamer17@gmail.com>
2024-07-13 16:57:07 +0200

    Reposition and add margins

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 const Card = (props) => {
     return (
         <div className='flex flex-col flex-wrap justify-center items-center m-10'>
-            <div className='size-1/2'>
+            <div className='size-5/12'>
                 <Link to={props.path}>
                     <img src={props.link} alt={props.alt} href={props.path} className='object-contain h-full w-full cursor-pointer'/>
                 </Link>
             </div>
-            <div className='flex flex-col w-1/2 h-1/3 pt-10 justify-between items-center'>
+            <div className='flex flex-col w-1/2 h-1/3 pt-10 my-10 justify-between items-center'>
                 <h1 className={`${props.gradient} text-transparent text-3xl font-bold animate-gradient`}>{props.source} {props.destination}</h1>
-                <Link to={props.path} className={`mt-6 py-4 pr-32 pl-32 text-zinc-100 text-transparent text-xl ${props.border} font-bold text-center rounded-full ${props.hover}`}>Go!</Link>
+                <Link to={props.path} className={`mt-10 py-4 pr-32 pl-32 text-zinc-100 text-transparent text-xl ${props.border} font-bold text-center rounded-full ${props.hover}`}>Go!</Link>
             </div>
         </div>
     );
