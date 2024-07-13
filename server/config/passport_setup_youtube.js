37726852e4001f1ec39404929694815e54b664eb
faticamer <faticamer17@gmail.com>
2024-07-13 16:33:18 +0200

    Add passport configuration for authenticating with Google

---
+++ server/config/passport_setup_youtube.js
@@ -0,0 +1,25 @@
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+  
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.YOUTUBE_CLIENT_ID,
+    clientSecret: process.env.YOUTUBE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5555/auth/youtube/callback",
+    prompt: 'consent',
+    scope: [
+        'https://www.googleapis.com/auth/youtubepartner',
+        'https://www.googleapis.com/auth/youtube',
+        'https://www.googleapis.com/auth/youtube.force-ssl'
+    ]
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    done(null, { accessToken, refreshToken, profile, cb })
+  }
+));
