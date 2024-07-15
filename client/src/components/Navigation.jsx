77af809923414cf11352d650e17b5e7f04918603
faticamer <faticamer17@gmail.com>
2024-07-15 21:53:39 +0200

    Re-design

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -7,10 +7,6 @@ import { Link } from 'react-router-dom'
 
 const NavigationBar = (props) => {
 
-    const openSourceCode = () => {
-        window.open('https://github.com/faticamer/playlist-converter')
-    }
-
     return (        
         <nav className='relative mx-auto p-3 text-white bg-spotifyDarkGrey'>
             <div className='flex items-center justify-between'>
@@ -19,22 +15,19 @@ const NavigationBar = (props) => {
                     {props.user ?
                         <div className='flex flex-row'>
                             <a href="/" className='text-3xl font-bold nunito-sans-bold ml-6'>Playlistify <span className='pl-2 text-md font-normal text-stone-500'> | </span><span className='pl-2 text-2xl font-normal'>Welcome, <span className='text-spotifyGreen'>{props.user}!</span></span></a>
-                            {props.profilePicture ?
-                                <Link to={props.profileUrl} target={`_blank`}>
-                                    <img src={props.profilePicture} alt="User Profile Picture" className='rounded-full mx-4 border-2 border-zinc-400'/>
-                                </Link> 
-                            :
-                                <div></div>
-                            }
                         </div>
                         :
                         <a href="/" className='text-3xl font-bold nunito-sans-bold ml-6'>Playlistify <span className='pl-2 text-md font-normal text-stone-500'> | </span><span className='pl-2 text-2xl font-normal'>Welcome, Guest!</span></a>
                     }
                 </div>
                 <div className='flex flex-row justify-between items-center gap-5 nunito-sans-regular'>
-                    <div onClick={openSourceCode}>
-                        <button className='flex flex-row justify-center items-center rounded-full outline outline-1 hover:outline-2 p-1'><img src={github} alt="GitHub Logo" className='w-8 h-8 mr-3'/>Source Code</button>
-                    </div>
+                    {props.profilePicture ?
+                        <Link to={props.profileUrl} target={`_blank`}>
+                            <img src={props.profilePicture} alt="User Profile Picture" className='rounded-full mx-4 border-2 border-zinc-600'/>
+                        </Link> 
+                    :
+                        <div></div>
+                    }
                     <ConnectButton
                         user={props.user} 
                         title="Spotify"
@@ -51,6 +44,5 @@ NavigationBar.propTypes = {
     profilePicture: PropTypes.string,
     profileUrl: PropTypes.string
 };
-  
 
 export default NavigationBar
