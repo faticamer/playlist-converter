7cbce4cbcdd75b1e166358fbcec1399b28eabe5d
faticamer <faticamer17@gmail.com>
2024-08-28 22:17:32 +0200

    Remove ConvertClone.jsx from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { SpotifyAuthProvider } from './context/SpotifyAuthContext'
 import Home from './pages/Home'
 import InfoWindow from './pages/InfoWindow'
 import Convert from './pages/Convert'
-import ConvertClone from './pages/ConvertClone'
 
 const App = () => {
 
@@ -16,7 +15,6 @@ const App = () => {
             <Route path='/' element={<Home />} />
             <Route path='/info' element={<InfoWindow />} />
             <Route path='/convert' element={<Convert />} />
-            <Route path='/convert-clone' element={<ConvertClone />} />
           </Routes>
         </SpotifyAuthProvider>
       </div>
